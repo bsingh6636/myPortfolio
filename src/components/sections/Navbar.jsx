12fa2fa88f7689c53b3c034243fe76df22706af8
Manmohan Kushwaha <mkush575@gmail.com>
2026-09-23 00:51:09 +0530

    space

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -5,7 +5,6 @@ import { Button } from '../ui/button';
 import { useTheme } from '../../contexts/ThemeContext';
 import { cn } from '../../lib/utils';
 
-
 const navLinks = [
   { name: 'About', href: '#about' },
   { name: 'Experience', href: '#experience' },
