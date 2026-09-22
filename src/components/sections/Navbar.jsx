7f0a04ad173f506e73fea82972cbaea837ce42a5
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2026-09-23 01:01:59 +0530

    test

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -6,6 +6,7 @@ import { useTheme } from '../../contexts/ThemeContext';
 import { cn } from '../../lib/utils';
 
 
+
 const navLinks = [
   { name: 'About', href: '#about' },
   { name: 'Experience', href: '#experience' },
